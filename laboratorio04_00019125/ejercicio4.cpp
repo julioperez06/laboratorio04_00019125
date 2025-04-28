7d06b3800e3c8b4030e893e2cfe72194654a0436
julio cesar perez salazar <00019125@uca.edu.sv>
2025-04-28 15:31:35 -0600

    Add files via upload

---
+++ laboratorio04_00019125/ejercicio4.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+// determinar variables
+
+{
+    int numero;
+
+    cout << " mucho gusto, ingresa un numero" << endl;
+
+    cin >> numero;
+
+    if (numero = 0)
+
+    {
+        cout << "el numero es cero" << endl;
+    }
+    else if (numero < 0)
+    {
+        cout << "el numero es negativo" << endl;
+    }
+    else if (numero > 0)
+    {
+        cout << "el numero es positivo" << endl;
+    }
+
+    return 0;
+}
